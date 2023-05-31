4d2c800d619c5b38972c40aed9df18abf712a080
nadel-la <nadella.ayudhia@gmail.com>
2023-06-01 03:07:01 +0700

    maps done 1, style unfinished

--- client/screens/addEventFormScreen.jsx
+++ client/screens/addEventFormScreen.jsx
@@ -1,40 +1,121 @@
 import React from "react";
-import MapView from "react-native-maps";
-import { Marker } from "react-native-maps";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+  TextInput,
+} from "react-native";
 
-import { Text, View, StyleSheet, Button, SafeAreaView } from "react-native";
+// Geocoder.init("AIzaSyDJCBwVAW27Z24KW63gvImv4NZVNIwaqSA");
+const GOOGLE_PLACES_API_KEY = "AIzaSyDJCBwVAW27Z24KW63gvImv4NZVNIwaqSA";
 
 export default function AddEventFormScreen() {
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <TextInput
-          placeholder="Search for an address"
-          placeholderTextColor="#000"
-          style={styles.searchBox}
-          onChangeText={(text) => this.searchLocation(text)}
-          value={this.state.searchKeyword}
-        />
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <GooglePlacesAutocomplete
+            placeholder="Enter Location"
+            minLength={2}
+            autoFocus={false}
+            returnKeyType={"default"}
+            fetchDetails={true}
+            styles={{
+              textInputContainer: {
+                backgroundColor: "grey",
+              },
+              textInput: {
+                height: 38,
+                color: "#5d5d5d",
+                fontSize: 16,
+              },
+              predefinedPlacesDescription: {
+                color: "#1faadb",
+              },
+            }}
+            query={{
+              key: GOOGLE_PLACES_API_KEY,
+              language: "en", // language of the results
+              components: "country:id",
+            }}
+            // onPress={(data, details = null) => console.log(data)}
+            onFail={(error) => console.error(error)}
+            onPress={(data, details = null) => {
+              const location = details.geometry.location;
+              setSelectedLocation({
+                latitude: location.lat,
+                longitude: location.lng,
+                address: data.description,
+              });
+              console.log(location);
+            }}
+          />
+          <Button
+            title="Add Location"
+            onPress={() => {
+              if (selectedLocation) {
+                console.log("Location added:", selectedLocation);
+                // Anda dapat mengirim data ini ke server atau menyimpannya dalam state atau konteks aplikasi Anda
+              } else {
+                console.log("No location selected");
+              }
+            }}
+          />
+        </View>
       </SafeAreaView>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  searchBox: {
-    width: 340,
-    height: 50,
-    fontSize: 18,
-    borderRadius: 8,
-    borderColor: "#aaa",
-    color: "#000",
-    backgroundColor: "#fff",
-    borderWidth: 1.5,
-    paddingLeft: 15,
-  },
   container: {
     flex: 1,
-    backgroundColor: "lightblue",
+    alignContent: "center",
+    justifyContent: "center",
+    marginTop: 120,
+  },
+  textInputContainer: {
+    flexDirection: "row",
+  },
+  textInput: {
+    backgroundColor: "#FFFFFF",
+    height: 44,
+    borderRadius: 5,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    fontSize: 15,
+    flex: 1,
+  },
+  poweredContainer: {
+    justifyContent: "flex-end",
     alignItems: "center",
+    borderBottomRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    borderColor: "#c8c7cc",
+    borderTopWidth: 0.5,
+  },
+  powered: {},
+  listView: {},
+  row: {
+    backgroundColor: "#FFFFFF",
+    padding: 13,
+    height: 44,
+    flexDirection: "row",
+  },
+  separator: {
+    height: 0.5,
+    backgroundColor: "#c8c7cc",
+  },
+  description: {},
+  loader: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    height: 20,
   },
 });
