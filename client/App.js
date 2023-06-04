8b8ab9599886972717cb38c8695db64ae897e120
Kasfi Tamiya <Kasfitamiya999@gmail.com>
2023-06-04 16:57:59 +0700

    .

--- client/App.js
+++ client/App.js
@@ -1,19 +1,15 @@
-<<<<<<< HEAD
 import "react-native-gesture-handler";
 import { React, useCallback, useState, useEffect } from "react";
 import { Text, View } from "react-native";
 import * as Font from "expo-font";
 import { useFonts } from "expo-font";
-=======
 import * as React from "react";
 import { SafeAreaView, Text, View } from "react-native";
->>>>>>> 67cca3c5794afdf0edadae83bd2c570015028167
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
 import ChatScreen from "./screens/chatScreen";
-<<<<<<< HEAD
 import HomeScreen from "./screens/homeScreen";
 import BoardingScreen from "./screens/boardingScreen";
 import LoginScreen from "./screens/loginScreen";
@@ -21,9 +17,6 @@ import RegisterScreen from "./screens/registerScreen";
 import WelcomeSport from "./screens/welcomeSportScreen";
 import WelcomeProfile from "./screens/welcomeProfileScreen";
 import WelcomeLevel from "./screens/welcomeLevelScreen";
-
-const Stack = createStackNavigator();
-=======
 import AddEventFormScreen from "./screens/addEventFormScreen";
 import CalendarTest from "./screens/calendarText";
 import HomeScreen from "./screens/homeScreen";
@@ -32,23 +25,7 @@ import UserProfile from "./screens/userProfileScreen";
 import ThankYouScreen from "./screens/thankYouScreen";
 import AdminReview from "./screens/reviewAdminScreen";
 
-// function HomeScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//       <Text>Home!</Text>
-//     </View>
-//   );
-// }
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-
->>>>>>> 67cca3c5794afdf0edadae83bd2c570015028167
+const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
@@ -70,10 +47,11 @@ export default function App() {
   }
 
   return (
-<<<<<<< HEAD
-    <View style={{ flex: 1 }}>
+
+
+    <SafeAreaView style={{ flex: 1 }}>
       <NavigationContainer>
-        <Stack.Navigator>
+      <Stack.Navigator>
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Register" component={RegisterScreen} />
           <Stack.Screen name="WelcomeSport" component={WelcomeSport} />
@@ -81,40 +59,6 @@ export default function App() {
           <Stack.Screen name="WelcomeLevel" component={WelcomeLevel} />
           <Stack.Screen name="onBoarding" component={BoardingScreen} />
         </Stack.Navigator>
-      </NavigationContainer>
-    </View>
-
-    // <NavigationContainer>
-    //   <Tab.Navigator
-    //     screenOptions={({ route }) => ({
-    //       tabBarIcon: ({ focused, color, size }) => {
-    //         let iconName;
-
-    //         if (route.name === "Home") {
-    //           iconName = focused
-    //             ? "ios-information-circle"
-    //             : "ios-information-circle-outline";
-    //         } else if (route.name === "Settings") {
-    //           iconName = focused ? "ios-list" : "ios-list-outline";
-    //         }
-
-    //         // You can return any component that you like here!
-    //         return <Ionicons name={iconName} size={size} color={color} />;
-    //       },
-    //     })}
-    //     tabBarOptions={{
-    //       style: {
-    //         backgroundColor: "#CEF249",
-    //       },
-    //     }}
-    //   >
-    //     <Tab.Screen name="Home" component={BoardingScreen} />
-    //     <Tab.Screen name="Settings" component={ChatScreen} />
-    //   </Tab.Navigator>
-    // </NavigationContainer>
-=======
-    <SafeAreaView style={{ flex: 1 }}>
-      <NavigationContainer>
         <Tab.Navigator
           screenOptions={({ route }) => ({
             headerShown: false,
@@ -147,6 +91,5 @@ export default function App() {
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaView>
->>>>>>> 67cca3c5794afdf0edadae83bd2c570015028167
   );
 }
